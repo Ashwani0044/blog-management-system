8798a9e77b899bfdfa55d7fff784a5d439ccf70c
Ashwani0044 <ashwanib707@gmail.com>
2026-07-24 20:56:40 +0530

    Day 2: Added Home and Add Blog HTML pages

--- blog-management-system/server.js
+++ blog-management-system/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.use(express.static("public"));
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!!");
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
