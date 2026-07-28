5198fbac3ebb1c0ccb7c53cb0964ed66b68bf087
Ashwani0044 <ashwanib707@gmail.com>
2026-07-28 19:34:00 +0530

    Day 6: Added blog creation API with validation

--- blog-management-system/routes/blogRoutes.js
+++ blog-management-system/routes/blogRoutes.js
@@ -1,31 +1,44 @@
 const express = require("express");
-const { desc } = require("framer-motion/client");
 
 const router = express.Router();
 
-// array to store blogs posted basically a temporary storage..
 let blogs = [];
 
+// GET All Blogs
 router.get("/", (req, res) => {
-    res.status(200).json(blogs);
+    res.status(200).json({
+        success: true,
+        count: blogs.length,
+        data: blogs
+    });
 });
 
+// POST Blog
 router.post("/", (req, res) => {
 
-    const {title, author, description} = req.body;
+    const { title, author, description } = req.body;
+
+    if (!title || !author || !description) {
+        return res.status(400).json({
+            success: false,
+            message: "All fields are required."
+        });
+    }
 
     const blog = {
-        id : Date.now(),
-        title, 
-        author, 
-        description
+        id: Date.now(),
+        title,
+        author,
+        description,
+        createdAt: new Date().toLocaleString()
     };
 
     blogs.push(blog);
 
     res.status(201).json({
-        message : "Blog added successfully!!",
-        blogs
+        success: true,
+        message: "Blog created successfully!",
+        data: blog
     });
 
 });
